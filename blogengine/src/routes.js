de5b9adb84ea57da6e670b4ef60f46bf2882a4da
Austin Harn <werewolfred1593@gmail.com>
2017-08-22 14:23:37 -0600

    changed products to blogs

--- blogengine/src/routes.js
+++ blogengine/src/routes.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Products from '@/components/Products'
-import Product from '@/components/Product' 
+import Blogs from '@/components/Blogs'
+import Blog from '@/components/Blog' 
 
 Vue.use(Router)
 
 var routes = new Router({
     routes:[
         { name:'Home', component:Home, path:'/'},
-        { name:'Products', component:Products, path:'/Products'},
-        { name:'Product', component:Product, path:'/Products/:ProductId'}
+        { name:'Blogs', component:Products, path:'/Blogs'},
+        { name:'Blog', component:Product, path:'/Blogs/:blogId'}
     ]
 })
 
